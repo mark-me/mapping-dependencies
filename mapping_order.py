33e23da861994515ffe3e4ceafcef2f9b8f8f36f
Mark Zwart <mark.zwart@pobox.com>
2025-04-04 00:12:07 +0200

    First commit

--- mapping_order.py
+++ mapping_order.py
@@ -1,6 +1,9 @@
 import json
 
+import igraph as ig
+import networkx as nx
 import pandas as pd
+from pyvis.network import Network
 
 from log_config import logging
 
@@ -9,9 +12,8 @@ logger = logging.getLogger(__name__)
 
 class MappingDependencyParser:
     def __init__(self):
-        self.mappings = []
-        self.df_nodes = pd.DataFrame()
-        self.df_links = pd.DataFrame()
+        self.nodes = []
+        self.links = []
 
     def load_RETW_file(self, file: str) -> bool:
         """Load a RETW json file
@@ -31,13 +33,48 @@ class MappingDependencyParser:
 
         if "Mappings" in dict_RETW:
             lst_mappings = dict_RETW["Mappings"]
-            self.mappings.append(lst_mappings)
             for mapping in lst_mappings:
                 self._add_mapping(dict_mapping=mapping)
         else:
             logger.warning(f"Couldn't find mappings in RETW file '{file}'")
 
         return True
+
+    def get_dag(self) -> ig.Graph:
+        """Turns mappings into a Directed Graph
+
+        Returns:
+            ig.Graph: Directed graph of the mappings and entities involved
+        """
+        graph = ig.Graph.DictList(edges=self.links, vertices=self.nodes, directed=True)
+        return graph
+
+    def plot_dag(self, dag: ig.Graph, file_png_out: str) -> None:
+        """Creates an image of the DAG
+
+        Args:
+            dag (ig.Graph): DAG of mappings and entities
+            file_png_out (str): file name to write the image to
+        """
+        node_colors = {
+            "source_entity": "yellow",
+            "mapping": "green",
+            "target_entity": "red",
+        }
+        dag.vs["color"] = [node_colors[type_key] for type_key in dag.vs["type"]]
+        dag.vs["label"] = dag.vs["Name"]
+        visual_style = {"bbox": (1920,1080), "layout": dag.layout("rt_circular"), "margin": 50}
+        ig.plot(dag, target=file_png_out, **visual_style)
+
+    def plot_dag_interactive(self, dag: ig.Graph, file_png_out: str) -> None:
+        df = pd.DataFrame(self.links)
+        #df.loc[df["ẗype"] == "mappings", "color"] = "green"
+        graph = nx.from_pandas_edgelist(df, )
+        nt = Network('1080px', '1920px', directed=True)
+        nt.from_nx(graph)
+        nt.toggle_physics(True)
+        nt.show(file_png_out, notebook=False)
+        pass
 
     def _add_mapping(self, dict_mapping: dict) -> bool:
         """Create nodes and edges based on mapping
@@ -46,19 +83,7 @@ class MappingDependencyParser:
             dict_mapping (dict): RETW mapping
         """
         # Create a node from the mapping information
-        keys_mapping = [
-            "Id",
-            "Name",
-            "Code",
-            "CreationDate",
-            "Creator",
-            "ModificationDate",
-            "Modifier",
-        ]
-        df_node = pd.DataFrame({key: dict_mapping[key] for key in keys_mapping})
-        self.df_nodes = pd.concat(
-            pd.concat([self.df_nodes, df_node], ignore_index=True)
-        )
+        node_mapping = self._create_mapping_node(dict_mapping)
 
         # Create nodes for target and source entities
         keys_entity = [
@@ -71,7 +96,7 @@ class MappingDependencyParser:
             "IsDocumentModel",
         ]
         if "EntityTarget" in dict_mapping:
-            self._create_target_nodes(
+            node_target = self._create_target_node(
                 dict_mapping["EntityTarget"], entity_keys=keys_entity
             )
         else:
@@ -80,44 +105,89 @@ class MappingDependencyParser:
             )
             return False
         if "SourceComposition" in dict_mapping:
-            df_sources = self._create_source_nodes(
+            nodes_source = self._create_source_nodes(
                 dict_mapping["SourceComposition"], entity_keys=keys_entity
             )
         else:
             return False
-        # dict_mapping[]
+
+        # Create links
+        self.links.append(
+            {"source": node_mapping["name"], "target": node_target["name"]}
+        )
+        links_source = [
+            {"source": node["name"], "target": node_mapping["name"]}
+            for node in nodes_source
+        ]
+        self.links = self.links + links_source
+
+        # Combine nodes
+        lst_tmp_nodes = self.nodes + nodes_source + [node_mapping] + [node_target]
+        # Make sure only unique nodes are added (entities can be part of multiple mappings)
+        self.nodes = list({v["name"]: v for v in lst_tmp_nodes}.values())
         return True
 
-    def _create_target_nodes(self, entity_target: dict, entity_keys: list) -> pd.DataFrame:
+    def _create_mapping_node(self, mapping: dict) -> dict:
+        """Create a node for the mapping
+
+        Args:
+            mapping (dict): Mapping data
+
+        Returns:
+            dict: Mapping data, excluding source and entity data
+        """
+        keys_mapping = [
+            "Id",
+            "Name",
+            "Code",
+            "CreationDate",
+            "Creator",
+            "ModificationDate",
+            "Modifier",
+        ]
+        node = {key: mapping[key] for key in keys_mapping}
+        node["name"] = node.pop("Id")
+        node["type"] = "mapping"
+        return node
+
+    def _create_target_node(self, entity_target: dict, entity_keys: list) -> dict:
         """_summary_
 
         Args:
             dict_entity_target (dict): Data on the target entity
             lst_entity_keys (list): Entity data to include in the node
         """
-        df_node = pd.DataFrame({key: entity_target[key] for key in entity_keys})
-        return df_node
+        node = {key: entity_target[key] for key in entity_keys if key in entity_target}
+        node["name"] = node.pop("Id")
+        node["type"] = "target_entity"
+        return node
 
-    def _create_source_nodes(self, entity_sources: dict, entity_keys: list) -> pd.DataFrame:
+    def _create_source_nodes(self, entity_sources: dict, entity_keys: list) -> list:
         """_summary_
 
         Args:
             dict_entity_target (dict): _description_
             lst_entity_keys (list): _description_
         """
+        lst_nodes = []
         for source in entity_sources:
             source_entity = source["Entity"]
-            df_node = pd.DataFrame({key: source_entity[key] for key in entity_keys})
-            self.df_nodes = pd.concat(
-                pd.concat([self.df_nodes, df_node], ignore_index=True)
-            )
+            node = {
+                key: source_entity[key] for key in entity_keys if key in source_entity
+            }
+            node["name"] = node.pop("Id")
+            node["type"] = "source_entity"
+            lst_nodes.append(node)
+        return lst_nodes
 
 
 if __name__ == "__main__":
-    lst_files_RETW = ["etl_templates/output/Usecase_Aangifte_Behandeling.json"]
+    lst_files_RETW = ["output/Usecase_Aangifte_Behandeling.json"]
     dep_parser = MappingDependencyParser()
 
     for file_RETW in lst_files_RETW:
         success = dep_parser.load_RETW_file(file=file_RETW)
         if success:
-            dep_parser.mappings
+            graph = dep_parser.get_dag()
+            dep_parser.plot_dag(graph, "output/dag.png")
+            dep_parser.plot_dag_interactive(graph, file_png_out="output/output.html")
