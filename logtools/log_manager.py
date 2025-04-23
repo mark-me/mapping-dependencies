12f48b499a31a74a11d47845b813f4441d6c57b2
Mark Zwart <mark.zwart@pobox.com>
2025-04-23 23:47:18 +0200

    Added return type

--- logtools/log_manager.py
+++ logtools/log_manager.py
@@ -12,5 +12,16 @@ issue_tracker = IssueTrackingHandler()
 logging.getLogger().addHandler(issue_tracker)
 
 
-def get_logger(name: str):
+def get_logger(name: str) -> logging.Logger:
+    """Retrieves a logger instance by name.
+
+    This function simplifies getting a logger and ensures consistent setup
+    with the project's logging configuration.
+
+    Args:
+        name: The name of the logger to retrieve.
+
+    Returns:
+        A logger instance with the specified name.
+    """
     return logging.getLogger(name)
